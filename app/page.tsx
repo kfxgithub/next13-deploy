b4b7704cb2f145413758e437c6d831366eb7316a
FLORES JIAN KYLE GARRIDO RP <21031786@myrp.edu.sg>
2024-07-31 06:02:30 +0800

    fix1.2

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
-import WaterGridDrop from "@/components/ui/WaterGridDrop";
+// import WaterGridDrop from "@/components/ui/WaterGridDrop";
 
 export default function Home() {
   return (
@@ -66,7 +66,7 @@ export default function Home() {
 
 
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
-      <div className="max-w-7xl w-full">
+      <div className="max-w-7xl w-full" >
         <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome />}]}/>
         <Hero />
         <Grid/>
